9e3cea21e6f406eb936456f4ca591d88e1c21c51
hong <1060850932@qq.com>
2020-09-23 14:38:38 +0800

    初始化vue-sun-ui使用指南项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入vue-sun-ui
+import SunUi from 'vue-sun-ui'
+// 导入vue-sun-ui样式
+import 'vue-sun-ui/dist/vue-sun-ui.css'
+
+// 安装SunUi
+Vue.use(SunUi)
 Vue.config.productionTip = false
 
 new Vue({
