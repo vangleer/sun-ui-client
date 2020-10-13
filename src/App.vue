4ab33e930e6a773b53bcfffcedba604f56cfa12a
hong <1060850932@qq.com>
2020-10-13 16:07:02 +0800

    。。。

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,9 @@
      watch:{
       $route(to,from){
         this.$refs.navRef.changeRouter()
-        this.childtPath = 'https://java0088.github.io/sun/dist/#'+to.path
+
+        this.childtPath = 'https://java0088.github.io/sun/dist/#'+to.path // 线上
+        // this.childtPath = 'http://localhost:8080/#'+to.path // 测试
       }
     },
     activated() {
